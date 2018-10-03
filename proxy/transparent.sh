b03ae6eba9d0dff4c12aef331c62b91ab53462e7
dummys <dummys1337@gmail.com>
2018-10-03 13:28:05 +0200

    changed the deprecated mode of mitmproxy

--- proxy/transparent.sh
+++ proxy/transparent.sh
@@ -4,9 +4,9 @@ iptables -t nat -A PREROUTING -i wlan1 -p tcp --dport 443 -j REDIRECT --to-port 
 ip6tables -t nat -A PREROUTING -i wlan1 -p tcp --dport 80 -j REDIRECT --to-port 8080
 ip6tables -t nat -A PREROUTING -i wlan1 -p tcp --dport 443 -j REDIRECT --to-port 8080
 echo "Use mitmproxy in transparent mode:"
-echo " * mitmproxy -T -z"
-echo " * mitmdump -T -z"
-echo " * mitmweb -T -z"
+echo " * mitmproxy -m transparent --anticomp"
+echo " * mitmdump -m transparent --anticomp"
+echo " * mitmweb -m transparent --anticomp"
 echo ""
 echo "To install the mitmproxy certificate, browse http://mitm.it on your target device."
 echo "For more details about mitmproxy, visit https://docs.mitmproxy.org"
