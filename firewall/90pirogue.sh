6e83783249d07a14d4791647d81633484060d595
aperault <anthon77360@hotmail.fr>
2018-11-26 16:20:09 +0100

    Update 90pirogue.sh

--- firewall/90pirogue.sh
+++ firewall/90pirogue.sh
@@ -5,43 +5,20 @@ STATUS=$2
 
 function add_rules {
         iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
-        iptables -A FORWARD -i $1 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-        iptables -A FORWARD -i wlan1 -o $1 -j ACCEPT
-        ip6tables -A FORWARD -i $1 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-        ip6tables -A FORWARD -i wlan1 -o $1 -j ACCEPT
+        iptables -A FORWARD -i $1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        iptables -A FORWARD -i wlan0 -o $1 -j ACCEPT
+        ip6tables -A FORWARD -i $1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        ip6tables -A FORWARD -i wlan0 -o $1 -j ACCEPT
 }
 
 function remove_rules {
         iptables -t nat -D POSTROUTING -o $1 -j MASQUERADE
-        iptables -D FORWARD -i $1 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-        iptables -D FORWARD -i wlan1 -o $1 -j ACCEPT
-        ip6tables -D FORWARD -i $1 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-        ip6tables -D FORWARD -i wlan1 -o $1 -j ACCEPT
+        iptables -D FORWARD -i $1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        iptables -D FORWARD -i wlan0 -o $1 -j ACCEPT
+        ip6tables -D FORWARD -i $1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        ip6tables -D FORWARD -i wlan0 -o $1 -j ACCEPT
 }
 
-if [ "$IF" == "wlan0" ]
-then
-        case "$2" in
-                up)
-                        # interface is up
-                        echo "wlan0 up" >> /tmp/pirogue_routing
-                        add_rules $IF
-                        ;;
-                down)
-                        # interface will be down
-                        echo "wlan0 down" >> /tmp/pirogue_routing
-                        remove_rules $IF
-                        ;;
-                pre-up)
-                        # interface will be up
-                        ;;
-                post-down)
-                        # interface is down
-                        ;;
-                *)
-                        ;;
-        esac
-fi
 
 if [ "$IF" == "eth0" ]
 then
